ad956eff9d024c6dc34a13160fd4ca227ed4e4ab
sergiovine <129095459+sergiovine@users.noreply.github.com>
2023-05-10 23:42:00 +0200

    Delete registrazione directory

--- registrazione/create_table.sql
+++
@@ -1,9 +0,0 @@
-create table utente (
-    email varchar(40),
-    nome varchar(40),
-    cognome varchar(40),
-    comune varchar(40),
-    via varchar(40),
-    paswd varchar(32) not null,
-    primary key (email)
-);
